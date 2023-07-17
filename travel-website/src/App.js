585eed38ad266b6ea802797fb9bfe9a56ea6d4d8
Naitikmp <90212814+Naitikmp@users.noreply.github.com>
2023-07-17 08:22:51 +0530

    Update App.js

--- travel-website/src/App.js
+++ travel-website/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
     <Routes>
-      <Route path="/home" element={<Home />}/>
+      <Route path="/" element={<Home />}/>
       <Route path="/about" element={<About />}/>
 
       <Route path="/service" element={<Service />}/>
